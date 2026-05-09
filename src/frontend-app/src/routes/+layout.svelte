f1dd8350f798411e45fdbbfe07354cc7f1c475b1
Vitória <vitoriaferreiradealmeida1230@gmail.com>
2026-05-09 16:21:17 -0300

    Finalizamos tela inicial e menu

--- src/frontend-app/src/routes/+layout.svelte
+++ src/frontend-app/src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 	let { children } = $props();
 </script>
 
-<main class="flex flex-col items-center justify-center min-h-screen w-full bg-white dark:bg-gray-900">
+<main class="flex flex-col items-center justify-center min-h-screen w-full bg-emerald-200 dark:bg-gray-900">
   <Menu />
   <div class="w-full max-w-3xl px-4 md:px-8">
     {@render children()}
