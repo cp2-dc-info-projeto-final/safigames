7d76de061283017fa4b7373068aa46e8af2b2e34
vitoria <vitoriaferreiradealmeida1230@gmail.com>
2026-05-09 11:47:01 -0300

    Fizemos tela inicial e menu

--- src/frontend-app/src/routes/+page.svelte
+++ src/frontend-app/src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
   import { Heading } from 'flowbite-svelte';
   import Menu from '../components/Menu.svelte';
-  import {  P, ImagePlaceholder, Skeleton, TextPlaceholder } from "flowbite-svelte";
+  import {  P } from "flowbite-svelte";
 </script>
 
 <Menu />
 
 <div class="text-center p-8 pt-32">
-	<Heading tag="h2" class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-6">Home</Heading>
+	<Heading tag="h2" class="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-6">Esgotamento: Um projeto Safigames</Heading>
 	<P class="text-lg leading-relaxed text-gray-700 dark:text-gray-300 mb-4 text-justify">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Mauris consequat, sapien et dictum cursus, urna erat laoreet enim, nec gravida urna erat at sapien. Etiam nec dolor vitae odio hendrerit venenatis. Proin nec turpis nec sem tempus dictum. Vivamus tincidunt, nisi nec facilisis cursus, enim erat dictum neque, at dignissim enim urna at velit. Integer non turpis id mi tristique cursus. Nullam in sem vitae massa dictum dictum.
+		Somos do Colégio Pedro II - Campus Duque de Caxias do terceiro ano do curso desenvolvimento de sistemas. 
+		Como projeto final, estamos desenvolvendo um jogo estilo RPG de texto. O jogo será baseado em botões para interagir com o mundo.
+		O jogo será dividido em episódios, e haverá ilustração no formato ASCII.
 	</P>
-	<Skeleton class="mt-16 mb-8" />
-	<ImagePlaceholder class="my-8" />
-	<TextPlaceholder class="my-8" />
+	<img src='../images/ilustracao-de-exemplo.png' alt="Ilustração de exemplo">
 </div>
